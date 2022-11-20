966256511d65b6f6a70c4e88f0ad7ca4dca1199a
Georg Hans <gehans@outlook.de>
2022-11-20 20:22:14 +0100

    add voting mechanism

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,31 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { useEffect } from "react";
-
+import { useState } from "react";
 import { trpc } from "../utils/trpc";
+const _MAX_VIDEOS_ = 8;
 
 const Home: NextPage = () => {
-  // const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
-  const videos = trpc.voting.getAllVideos.useQuery();
+  const allVideos = trpc.voting.getAllVideos.useQuery();
+  const vote = trpc.voting.castVote.useMutation();
+  const [videosInVoting, setVideosInVoting] = useState([0, 1]);
 
-  useEffect(() => {
-    videos.data && console.log(videos.data);
-  }, [videos.data]);
+  const getRandomVideoPair = () => {
+    const video1 = Math.floor(Math.random() * _MAX_VIDEOS_);
+    let video2 = Math.floor(Math.random() * _MAX_VIDEOS_);
+    while (video2 === video1) {
+      video2 = Math.floor(Math.random() * _MAX_VIDEOS_);
+    }
+    return [video1, video2];
+  };
+
+  const castVote = (votedFor: number, votedAgainst: number) => {
+    vote.mutate({ votedFor, votedAgainst });
+    setVideosInVoting(getRandomVideoPair());
+  };
+
+  if (!allVideos.data) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
@@ -25,11 +40,27 @@ const Home: NextPage = () => {
             <span className="text-[hsl(280,100%,70%)]">Voty</span>
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20">
-              <h3 className="text-2xl font-bold">Video 1</h3>
+            <div
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              onClick={() =>
+                castVote(videosInVoting[0] ?? 0, videosInVoting[1] ?? 0)
+              }
+            >
+              <h3 className="text-2xl font-bold">
+                {"Video 1: " + allVideos.data[videosInVoting[0] ?? 0]?.title ??
+                  "Loading..."}
+              </h3>
             </div>
-            <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20">
-              <h3 className="text-2xl font-bold">Video 2</h3>
+            <div
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              onClick={() =>
+                castVote(videosInVoting[1] ?? 0, videosInVoting[0] ?? 0)
+              }
+            >
+              <h3 className="text-2xl font-bold">
+                {"Video 1: " + allVideos.data[videosInVoting[1] ?? 0]?.title ??
+                  "Loading..."}
+              </h3>
             </div>
           </div>
         </div>
