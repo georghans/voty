6df144a30d210a32716e516590f1cfa5a3c5a946
Georg Hans <gehans@outlook.de>
2022-11-21 14:16:20 +0100

    infer video endpoint type

--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -1,5 +1,7 @@
+import type { appRouter } from "./router/_app";
 import { initTRPC } from "@trpc/server";
 import superjson from "superjson";
+import type { inferRouterOutputs } from "@trpc/server";
 
 import { type Context } from "./context";
 
@@ -11,5 +13,8 @@ const t = initTRPC.context<Context>().create({
 });
 
 export const router = t.router;
+
+type RouterOutput = inferRouterOutputs<typeof appRouter>;
+export type VideoFromServer = RouterOutput["voting"]["getVideoPair"];
 
 export const publicProcedure = t.procedure;
