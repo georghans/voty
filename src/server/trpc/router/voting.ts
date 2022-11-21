0ff17977ad855c5f142f9ab4d4c3f2f43dcd4cbd
Georg Hans <gehans@outlook.de>
2022-11-21 10:44:01 +0100

    getVideoPair endmpoint

--- src/server/trpc/router/voting.ts
+++ src/server/trpc/router/voting.ts
@@ -1,9 +1,20 @@
+import { getOptionsForVote } from "./../../../utils/getRandomVideo";
 import { z } from "zod";
 import { router, publicProcedure } from "../trpc";
 
 export const votingRouter = router({
   getAllVideos: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.video.findMany();
+  }),
+  getVideoPair: publicProcedure.query(async ({ ctx }) => {
+    const [first, second] = getOptionsForVote();
+    const bothVideos = await ctx.prisma.video.findMany({
+      where: {
+        OR: [{ id: first }, { id: second }],
+      },
+    });
+
+    return { first: bothVideos[0], second: bothVideos[1] };
   }),
   castVote: publicProcedure
     .input(
