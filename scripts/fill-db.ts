095aa4a53ade30f1aa9047f37aa502c49c49a9b0
g hans <gehans@outlook.de>
2022-11-22 13:02:23 +0100

    add s3 endpoints to db

--- scripts/fill-db.ts
+++ scripts/fill-db.ts
@@ -4,35 +4,35 @@ const fillVideoTable = async () => {
   const videos = [
     {
       title: "Cheng Goal 1",
-      url: "/1_Cheng1.mp4",
+      url: "https://s3.eu-central-1.amazonaws.com/voty.app/1_Cheng1.mp4",
     },
     {
       title: "Cheng Goal 2",
-      url: "/2_Cheng2.mp4",
+      url: "https://s3.eu-central-1.amazonaws.com/voty.app/2_Cheng2.mp4",
     },
     {
       title: "Penalty Goal",
-      url: "/3_Elfer.mp4",
+      url: "https://s3.eu-central-1.amazonaws.com/voty.app/3_Elfer.mp4",
     },
     {
       title: "Krämer Goal",
-      url: "/4_Krämer.mp4",
+      url: "https://s3.eu-central-1.amazonaws.com/voty.app/4_Krämer.mp4",
     },
     {
       title: "Nico Goal 1",
-      url: "/5_Nico.mp4^",
+      url: "https://s3.eu-central-1.amazonaws.com/voty.app/5_Nico.mp4",
     },
     {
       title: "Nico Goal 2",
-      url: "/6_Nico2.mp4",
+      url: "https://s3.eu-central-1.amazonaws.com/voty.app/6_Nico2.mp4",
     },
     {
       title: "Piet Goal",
-      url: "/7_Piet.mp4",
+      url: "https://s3.eu-central-1.amazonaws.com/voty.app/7_Piet.mp4",
     },
     {
       title: "Valdrin Goal",
-      url: "/8_Valdrin.mp4",
+      url: "https://s3.eu-central-1.amazonaws.com/voty.app/8_Valdrin.mp4",
     },
   ];
 
